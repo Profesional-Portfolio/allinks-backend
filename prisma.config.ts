281a518770fdbdf69aecea0de567512f28b0cfed
davepm <67056846+Deivid182@users.noreply.github.com>
2026-03-11 01:05:57 -0600

    feat: Configure Prisma for production builds by conditionally loading dotenv, including its config in tsconfig, and ignoring the dist folder.

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,15 @@
-import 'dotenv/config';
 import { defineConfig, env } from 'prisma/config';
 
+if (process.env.NODE_ENV !== 'production') {
+  await import('dotenv/config');
+}
+
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-  },
   datasource: {
     url: env('DATABASE_URL'),
   },
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  schema: 'prisma/schema.prisma',
 });
